95b72d4d9aee24b8b7e6648fbd0779eaa51410b9
william ikeji <williamikeji@gmail.com>
2020-03-08 00:04:58 +0100

    set up validation completed

--- app/renderer.js
+++ app/renderer.js
@@ -1,13 +1,19 @@
 //import db file
-const Database = require("../src/js/db");
-const db = new Database();
+const Validator = require("../src/js/validator");
+
+//instantiate classes
+const validate = new Validator();
 
 //details store
 let details = {
   companyName: "",
   address: "",
   companyId: "",
-  branchId: ""
+  branchId: "",
+  manager_firstname: "",
+  manager_lastname: "",
+  manager_password: "",
+  manager_email: ""
 };
 
 // eslint-disable-next-line no-unused-vars
@@ -127,4 +133,84 @@ const showManagerDetail = e => {
     let error = "Please sellect a package";
     displayError(errorDiv, error);
   }
+};
+
+//process managers details
+// eslint-disable-next-line no-unused-vars
+const enterDetails = e => {
+  e.preventDefault();
+  //get error div
+  let errorDiv = document.getElementsByClassName("warning")[0];
+  //hide error box
+  if (!errorDiv.classList.contains("hide")) {
+    errorDiv.classList.add("hide");
+  }
+
+  //get all fields
+  let fname = document.getElementById("fname");
+  let lname = document.getElementById("lname");
+  let email = document.getElementById("email");
+  let pwd = document.getElementById("pwd");
+  let secPwd = document.getElementById("confirmPwd");
+  let inputs = [fname, lname, email, pwd, secPwd];
+
+  //check fields
+  if (validate.isEmpty(inputs)) {
+    displayError(errorDiv, "Please fill all fields");
+  }
+
+  //check if alpha only
+  else if (validate.isNotAlpha(fname.value.trim())) {
+    displayError(errorDiv, "First name should have alphabets only");
+  }
+
+  //check if alpha only
+  else if (validate.isNotAlpha(lname.value.trim())) {
+    displayError(errorDiv, "Last name should have alphabets only");
+  }
+
+  //validate if email
+  else if (validate.isNotEmail(email.value.trim())) {
+    displayError(errorDiv, "Email is not valid");
+  }
+
+  //validate password
+  //let returnedValue = validate.validPassword(pwd.value.trim());
+  else if (validate.notValidPassword(pwd.value.trim())) {
+    displayError(
+      errorDiv,
+      "Invalid password!!! Password should have an uppercase, a lowercase, a number, a special character and a minimum of six values "
+    );
+  } else if (pwd.value != secPwd.value) {
+    displayError(errorDiv, "Passwords do not match ");
+  } else {
+    //store details in the details object
+    details.manager_firstname = fname.value.trim();
+    details.manager_lastname = lname.value.trim();
+    details.manager_password = pwd.value.trim();
+    details.manager_email = email.value.trim();
+
+    //create database
+    validate.createSetup(details);
+  }
+};
+
+//compare the two passwords
+// eslint-disable-next-line no-unused-vars
+const comparePassword = e => {
+  let pwd = document.getElementById("pwd");
+  let secPwd = e.target;
+  if (pwd.value != secPwd.value) {
+    secPwd.style.border = "1px solid red";
+  } else {
+    secPwd.style.border = "1px solid green";
+  }
+};
+
+//empty confirmation password
+// eslint-disable-next-line no-unused-vars
+const emptySecPassword = e => {
+  let secPwd = document.getElementById("confirmPwd");
+  secPwd.value = "";
+  secPwd.style.border = "none";
 };
